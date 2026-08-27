c740fc3b0a017099c3cd5546931ca32a69648fe1
DBDA74 <DBDA74@ACTS.CDACECBLR.COM>
2026-08-27 17:58:33 +0530

    added palindromë checker from day 2

--- Day_02/assignment/exercise_1_palindrome_checker.py
+++ Day_02/assignment/exercise_1_palindrome_checker.py
@@ -5,10 +5,19 @@ Write a function that checks whether a given string is a palindrome (reads the s
 * **Sample Input**: `"A man a plan a canal Panama"`
 * **Sample Output**: `True`
 """
-
 def solve():
-    # TODO: Implement your solution here
-    pass
+    a = input("Enter the string: ")
+
+    a = ''.join(char.lower() for char in a if char.isalnum())
+    b = a[::-1]
+
+    if a == b:
+        print("True")
+    else:
+        print("False")
+
+    
+
 
 if __name__ == "__main__":
     solve()
