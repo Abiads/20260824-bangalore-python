c5b1fefa163b09c3ef66fcbef4f176df3f6169c5
DBDA74 <DBDA74@ACTS.CDACECBLR.COM>
2026-08-28 12:38:37 +0530

    Assignment Day 2 4 is completed

--- Day_02/assignment/exercise_04_vowel_consonant_frequency.py
+++ Day_02/assignment/exercise_04_vowel_consonant_frequency.py
@@ -6,9 +6,9 @@ Write a program that prompts the user to enter a string and counts:
 1. The individual frequency of each vowel (`a`, `e`, `i`, `o`, `u`), case-insensitively.
 2. The total count of all consonants.
 
-- **Sample Input**: `"Vinod Kumar Kayartaya"`
+- **Sample Input**: `"VinodKumarKayartaya"`
 - **Sample Output**:
-  ```text
+  ```text key val 
   Vowel Frequencies:
   a: 4
   e: 0
@@ -30,13 +30,47 @@ Write a program that prompts the user to enter a string and counts:
 📋 Step-by-Step Logic:
 1. Convert input string to lowercase: `text = text.lower()`.
 2. For vowels: loop over `'aeiou'` and get frequency using `text.count(v)`.
+
 3. For consonants: loop through each character in `text`. If `char.isalpha()` and `char not in 'aeiou'`, increment consonant counter.
 4. Display the formatted vowel frequencies and total consonants count.
 """
 
 def solve():
-    # TODO: Implement your solution following the hints above
-    pass
+  str1=str(input("Enter a string: "))
+
+  lower1=str1.lower()
+  n=len(lower1)
+  print(n)
+  
+  
+  acount=0
+  ecount=0
+  icount=0
+  ocount=0
+  ucount=0
+  for i in range (n):
+    
+    if 'a' in lower1[i]:
+      acount+=1
+    if 'e' in lower1[i]:
+      ecount+=1
+    if 'i' in lower1[i]:
+      icount+=1
+    if 'o' in lower1[i]:
+      ocount+=1
+    if 'u' in lower1[i]:
+      ucount+=1
+                           
+    
+
+  print("a:",acount)
+  print("e:",ecount)
+  print("i:",icount)
+  print("o",ocount)
+  print("u:",ucount)
+  vowelcount=  acount+ecount+icount+ocount+ucount
+  print("TotalConsonant",n-vowelcount)     
+
 
 if __name__ == "__main__":
     solve()
