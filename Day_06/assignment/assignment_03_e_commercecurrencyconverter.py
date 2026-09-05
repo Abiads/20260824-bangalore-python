141b4d904fe88903792a9059a03850fda8f07235
DBDA74 <DBDA74@ACTS.CDACECBLR.COM>
2026-09-05 14:46:41 +0530

    day 06 assingment 3

--- Day_06/assignment/assignment_03_e_commercecurrencyconverter.py
+++ Day_06/assignment/assignment_03_e_commercecurrencyconverter.py
@@ -39,9 +39,52 @@ except ValueError as e:
 ```
 """
 
-def solve():
-    # TODO: Implement your solution here
-    pass
+class PriceAmount:
+    
+    def __init__(self, value:float, currency : str):
+      self.value = value
+      self.currency = currency.upper()
 
-if __name__ == "__main__":
-    solve()
+    def __str__(self):
+      return f'{self.currency} {self.value:.2f}'
+
+    def __repr__(self):
+      return f"PriceAmount {self.value:.2f} {self.currency}"
+
+    def __add__(self, other):
+      if not isinstance(other, PriceAmount):
+        return NotImplemented
+      if self.currency != other.currency:
+         raise ValueError(f'Cannot add price amounts with different currencies: {self.currency} and {other.currency}.')
+
+      return PriceAmount(self.value + other.value, self.currency)
+
+    def __eq__(self, other):
+       
+       if self.currency == other.currency and self.value == other.value:
+          return True
+       
+       return False
+    
+    #   if not isinstance(other, PriceAmount):
+    #      return False
+      
+    #   return (self.currency == other.currency and self.value == other.value)
+          
+
+p1 = PriceAmount(19.99, "usd")
+p2 = PriceAmount(10.01, "USD")
+p3 = PriceAmount(15.00, "EUR")
+
+print(str(p1))      # Output: USD 19.99
+print(repr(p1))     # Output: PriceAmount(value=19.99, currency='USD')
+
+total = p1 + p2
+print(str(total))   # Output: USD 30.00
+
+print(p1 == PriceAmount(19.99, "eur")) # Output: True
+
+try:
+    bad_addition = p1 + p3
+except ValueError as e:
+    print(e)  # Output: Cannot add price amounts with different currencies: 'USD' and 'EUR'.
